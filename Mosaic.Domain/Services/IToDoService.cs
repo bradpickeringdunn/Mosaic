6c20529844b8088a47ed570eaa0465efeeb0a6a6
Howard PickeringDunn <hpicke201@emea.comcast.com>
2019-04-08 05:57:05 +0100

    Checked in missing domain

---
+++ Mosaic.Domain/Services/IToDoService.cs
@@ -0,0 +1,18 @@
+﻿using Mosaic.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mosaic.Domain.Services
+{
+    public interface IToDoService
+    {
+        Task MarkAsDone(int id, bool status, CancellationToken cancellationToken);
+        Task RemoveTask(int id, CancellationToken cancellationToken);
+        Task AddTask(string name, CancellationToken cancellationToken);
+        Task<IDictionary<int, ToDoItemDTO>> LoadItems(CancellationToken cancellationToken);
+    }
+}
---
+++ Mosaic.Domain/Services/ToDoService.cs
@@ -0,0 +1,40 @@
+﻿using Mosaic.DTO;
+using Mosaic.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mosaic.Domain.Services
+{
+    public class ToDoService : IToDoService
+    {
+        private readonly IToDoRepository _toDoRepository;
+        public ToDoService(IToDoRepository toDoRepository)
+        {
+            _toDoRepository = toDoRepository;
+        }
+
+        public async Task AddTask(string name, CancellationToken cancellationToken)
+        {
+            await _toDoRepository.AddTask(new ToDoItemDTO(name), cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task<IDictionary<int, ToDoItemDTO>> LoadItems(CancellationToken cancellationToken)
+        {
+            return await _toDoRepository.LoadTasks(cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task MarkAsDone(int id, bool status, CancellationToken cancellationToken)
+        {
+            await _toDoRepository.MarkAsDone(id, status, cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task RemoveTask(int id, CancellationToken cancellationToken)
+        {
+            await _toDoRepository.RemoveTask(id, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
